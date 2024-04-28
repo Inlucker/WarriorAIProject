c836937f8da1a4ac5570096ccaac3ec1be1ff718
JokerBagins <157324348+JokerBagins@users.noreply.github.com>
2024-04-28 12:27:34 +0300

    added third person template project

---
+++ Source/WarriorAIProject/WarriorAIProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WarriorAIProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WarriorAIProject, "WarriorAIProject" );
+ 
---
+++ Source/WarriorAIProject/WarriorAIProjectGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WarriorAIProjectGameMode.h"
+#include "WarriorAIProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AWarriorAIProjectGameMode::AWarriorAIProjectGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
