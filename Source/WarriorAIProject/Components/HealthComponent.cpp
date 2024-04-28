6c528d9845e7d406ce08da92c18e24b68641efb8
JokerBagins <157324348+JokerBagins@users.noreply.github.com>
2024-04-28 13:33:07 +0300

    Added HealthComponent class

---
+++ Source/WarriorAIProject/Components/HealthComponent.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent() : isDead(false)
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+void UHealthComponent::CheckIsDead()
+{
+	if (currentHealth <= 0.0f && !isDead)
+	{
+		isDead = true;
+		OnDeath.Broadcast();  // ֲûחמג סמבûעטÿ סלונעט
+	}
+}
+
+float UHealthComponent::GetHealth() const
+{
+	return currentHealth;
+}
+
+float UHealthComponent::GetMaxHealth() const
+{
+	return maxHealth;
+}
+
+void UHealthComponent::TakeDamage(float damageAmount)
+{
+	currentHealth -= std::min(damageAmount, currentHealth);
+	CheckIsDead();
+}
+
+void UHealthComponent::Heal(float healAmount)
+{
+	CheckIsDead();
+	if (!isDead)
+		currentHealth = std::min(currentHealth + healAmount, maxHealth);
+}
+
+bool UHealthComponent::IsDead() const
+{
+	return isDead;
+}
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/WarriorAIProject/Components/HealthComponent.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class WARRIORAIPROJECT_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Health")
+	float currentHealth;
+
+	UPROPERTY(EditAnywhere, Category = "Health")
+	float maxHealth;
+
+	UPROPERTY(EditAnywhere, Category = "Health")
+	bool isDead;
+
+	void CheckIsDead();
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	float GetHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	float GetMaxHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void TakeDamage(float damageAmount);
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void Heal(float healAmount);
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	bool IsDead() const;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathDelegate);
+	UPROPERTY(BlueprintAssignable, Category = "Health")
+	FOnDeathDelegate OnDeath;
+};
