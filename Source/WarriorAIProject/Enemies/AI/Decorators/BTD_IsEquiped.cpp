ced1c7039c10049428de40b8f64ee36c85d51741
JokerBagins <157324348+JokerBagins@users.noreply.github.com>
2024-04-28 22:00:31 +0300

    Added IsEquiped Decorator

---
+++ Source/WarriorAIProject/Enemies/AI/Decorators/BTD_IsEquiped.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTD_IsEquiped.h"
+#include "AIController.h"
+#include "../../../Components/WeaponComponent.h"
+
+bool UBTD_IsEquiped::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+  APawn* Pawn = OwnerComp.GetAIOwner()->GetPawn();
+  if (Pawn)
+  {
+    UWeaponComponent* WeaponComp = Pawn->FindComponentByClass<UWeaponComponent>();
+    if (WeaponComp)
+      return WeaponComp->IsEquiped();
+  }
+  return false;
+}
