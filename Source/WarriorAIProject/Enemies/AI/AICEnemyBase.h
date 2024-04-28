d8d1889a147b65e1a4303a40ad410d62ee96ba49
JokerBagins <157324348+JokerBagins@users.noreply.github.com>
2024-04-28 17:44:28 +0300

    Added AICEnemyBase and Blueprint class to configure it.

---
+++ Source/WarriorAIProject/Enemies/AI/AICEnemyBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AICEnemyBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIORAIPROJECT_API AAICEnemyBase : public AAIController
+{
+	GENERATED_BODY()
+
+};
