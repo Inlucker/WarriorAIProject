8cd0a83662f2eb6b71f260130a953d9971286c80
JokerBagins <157324348+JokerBagins@users.noreply.github.com>
2024-04-28 15:33:25 +0300

    Added BP_EnemyHPWidget

--- Source/WarriorAIProject/Widgets/PlayerHPWidget.h
+++ Source/WarriorAIProject/Widgets/PlayerHPWidget.h
@@ -16,14 +16,11 @@ class WARRIORAIPROJECT_API UPlayerHPWidget : public UUserWidget
 	GENERATED_BODY()
 	
 protected:
-	// Begin Play alternative
 	virtual void NativeConstruct() override;
 
-	/** Function to calculate Player HP Percentage */
 	UFUNCTION(BlueprintPure)
 	float CalculateHealthPercentage();
 
-	/** The player */
 	UPROPERTY(VisibleAnywhere)
 	AWarriorAIProjectCharacter* Player;
 };
